e0f77e031e1835e2a7d7281cfe2904eeb48ab717
Ariel <ariel.kuzminski@gmail.com>
2017-12-30 01:54:31 +0100

    switched backend from php and mysql to firebase, got rid of unused angular library

--- PizzaClicker/js/ajax.js
+++ PizzaClicker/js/ajax.js
@@ -1,105 +1,79 @@
 $(document).ready(function(){
-    
-    
+
     var params = {
-        action: 'add',
         name : name,
         score : parseInt($('#score').text())
-
     }
-    
+
     $('#myBtn').on('click', function() {
         getScores(params);
-        
     });
-    
+
+    let database = firebase.database();
 
 	function getScores(){
-        
-        $.ajax({
-            
-            url: 'json.php',
-            method: 'GET',
-            data: {
-                action: 'show'
-            },
-            success: function(received) {
-                //alert('AJAX POBIERANIE dziala');
-                console.log(received);
-                createList(received);
-            },
-            error: function(error) {
-                //alert('AJAX POBIERANIE nie dziala');
-                console.log(error);
-            }
+        let scoreReference = firebase.database().ref();
+        scoreReference.on('value', (snapshot) => {
+            let received = snapshot.val();
+            received = Object.values(received);
+            received.sort((a, b) => b.score - a.score);
+            createList(received);
         });
     };
-        
-        
+
     function setScores(params) {
-        
-        $.ajax({
-            url: 'json.php',
-            method: 'GET',
-            data: params,
-            success: function() {
-			    //alert('Ajax WYSYŁANIE dziala');
-                
-            },
-            error: function() {
-                //alert('Ajax WYSYŁANIE nie dziala');
-            }
-            
+        firebase.database().ref().push({
+            username: params.name,
+            score: params.score
         });
-    };  
-    
-    
+    };
+
     function createList (received) {
-        
+
         $('.modal-content').html('');
-        
+
         var p = '';
         $('.modal-content').append(p);
             p += '<div class="leaderboard">';
             p += '<div class="col-xs-4 userPosition">';
             p += 'Pozycja:';
             p += '<ul class="list-unstyled">';
-            for(var i = 1; i <= received.posts.length; i++) {
+            for(var i = 1; i <= 10; i++) {
                 p += '<li>' + i + '.' + '</li>';
             }
             p += '</ul>';
             p += '</div>';
-        
+
             p += '<div class="col-xs-4 userName">';
             p += 'Gracz:';
             p += '<ul class="list-unstyled">';
-            for(var i = 0; i < received.posts.length; i++) {
-                p += '<li>' + received.posts[i].name + '</li>';
+            for(var i = 0; i < 10; i++) {
+                p += '<li>' + received[i].username + '</li>';
             }
             p += '</ul>';
             p += '</div>';
-        
+
             p += '<div class="col-xs-4 userScore">';
             p += 'Wynik:';
             p += '<ul class="list-unstyled">';
-            for(var i = 0; i < received.posts.length; i++) {
-                p += '<li>' + received.posts[i].score + '</li>';
+            for(var i = 0; i < 10; i++) {
+                p += '<li>' + received[i].score + '</li>';
             }
             p += '</ul>';
             p += '</div>';
-        
+
             $('.modal-content').append(p);
     };
-    
-    
+
+
     $('#save').on('click', function() {
         params.name = ($('#name').val());
         params.score = parseInt($('#score').text());
-        
+
         if(params.name && params.score) {
             setScores(params);
         }
     });
-    
-    
+
+
 });
